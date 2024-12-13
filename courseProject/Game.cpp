ed763d6d411203f295d6cea717ffade6ea298ccc
Gabe-Hog <gabrielhoglund@hotmail.com>
2024-12-13 15:51:53 +0100

    started creating a game file

---
+++ courseProject/Game.cpp
@@ -0,0 +1,8 @@
+#include "Game.h"
+
+void Game::run()
+{
+	window.create(sf::VideoMode(800,800), "test");
+}
+
+
---
+++ courseProject/Game.h
@@ -0,0 +1,26 @@
+#ifndef GAME_H
+#define GAME_H
+#include "SFML/Graphics.hpp"
+
+
+using namespace std;
+
+class Game
+{
+private:
+	sf::RenderWindow window;;
+	
+
+
+public:
+
+	Game() = default;
+	void run();
+	
+	
+
+
+
+};
+
+#endif
